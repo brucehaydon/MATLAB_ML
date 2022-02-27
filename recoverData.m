260e25c5ece47a2456e705b1a73422d61adb6564
Bruce Haydon <100446562+brucehaydon@users.noreply.github.com>
2022-02-26 23:18:32 -0500

    Add files via upload

--- recoverData.m
+++ recoverData.m
@@ -2,8 +2,7 @@ function X_rec = recoverData(Z, U, K)
 %
 %   RECOVERDATA Recovers an approximation of the original
 %   data when using the projected data
-%   Bruce Haydon (New York, New York)
-%
+
 %   "X_rec" = RECOVERDATA(Z, U, K) recovers an approximation the 
 %   original data that has been reduced to K dimensions. It returns the
 %   approximate reconstruction in X_rec.
@@ -17,7 +16,7 @@ function X_rec = recoverData(Z, U, K)
 X_rec = zeros(size(Z, 1), size(U, 1));
 
 % ============================================
-% Description:  Computes the approximation of the data by projecting back
+% Description:  Compute the approximation of the data by projecting back
 %               onto the original space using the top K eigenvectors in U.
 %
 %               For the i-th example Z(i,:), the (approximate)
